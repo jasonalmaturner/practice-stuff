d7abc208003ad0d37c6aa8ffaf525c8f809b24f0
jasonalmaturner <jasonalmaturner@gmail.com>
2014-09-22 09:25:30 -0600

    Reversed string without using .reverse()

---
+++ main.js
@@ -0,0 +1,8 @@
+var reverseArray = function(str){
+	var newArr = str.split('');
+	var reverseArray = [];
+	for(var i = 0; i < newArr.length; i++;){
+		reverseArray.push[newArr.length - 1];
+	}
+	reverseArray.join('');
+}
