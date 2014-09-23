9833f81892669d31fac8e60626c56988304b646e
jasonalmaturner <jasonalmaturner@gmail.com>
2014-09-23 09:23:19 -0600

    added another toy problem

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ var reverseArray = function(str){
 }
 var myName = 'Jason Alma Turner';
 alert(reverseArray(myName));
+
+var factorial = function(num){
+    if(num === 0){
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+};
+alert(factorial(7));
+/*
+var factorial =  function(x){
+    var y
+    for(var i = x; i > x; i--){
+        var z = x * (x - 1);
+        y = z + y;
+    }
+    return y;
+}
+alert(factorial(5));*/
+//This above function needs tweeked to work
