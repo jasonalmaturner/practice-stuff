b614d19b10412ec8ac88613139f238406a480406
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-15 09:47:21 -0600

    Completed another toy problems

--- main.js
+++ main.js
@@ -146,3 +146,121 @@ var parenBit = function (str) {
 };
 
 alert(parenBit("xyz(abc)123"));
+
+var flatten = function (arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr.isArray(arr[i]) === false) {
+            newArr.push(arr[i]);
+        } else {
+            flatten(arr[i]);
+        }
+    }
+    return newArr;
+};
+
+console.log(flatten([1, 2, [3, [4], 5, 6], 7]);
+
+var flatten = function (arr) {
+    var newArr = [];
+    if (arr.isArray(arr[0]) === false) {
+        newArr.push(arr[0]);
+        arr.splice(0,1);
+        flatten(arr[0]);
+    } else if (arr.isArray(arr[0]) === true) {
+        flatten(arr[0]);
+    }/* else if (arr.isArry(arr[0]) === null) {
+        break;
+    }*/
+    return newArr;
+};
+
+console.log(flatten([1, 2, [3, [4], 5, 6], 7]));
+
+var animals = [
+    {
+        name: 'Dog',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Canidae',
+        species: 'Canis lupus'
+    },
+    {
+        name: 'Cat',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Felidae',
+        species: 'Felis catus'
+    },
+    {
+        name: 'Hippopotamus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Artiodactyla',
+        family: 'Hippopotamidae',
+        species: 'Hippopotamus amphibius'
+    },
+    {
+        name: 'Playtpus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Monotremata',
+        family: 'Ornithorhynchidae',
+        species: 'Ornithorhynchus anatinus'
+    },
+    {
+        name: 'Lizard',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Reptilia',
+        order: 'Squamata',
+        family: 'Agamidae',
+        species: 'Pogona vitticeps'
+    },
+    {
+        name: 'Blackbird',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Aves',
+        order: 'Passeriformes',
+        family: 'Turdidae',
+        species: 'Turdus merula'
+    }
+];
+    
+//given the above object, write a function that returns an object whose keys are the fields found above and whose values are objects containing all the values as keys and a count for how many times that value occurs
+
+//e.g.
+//{
+//    kingdom: {Animalia: 6}
+//    phylum: {Chordata: 6},
+//    class: {Mammalia: 4, Reptilia: 1, Aves: 1}
+//}
+
+//code here
+
+var totalTaxa = function(arr){
+    var total = {};
+    for(var i = 0; i < arr.length; i++){
+        for(var prop in arr[i]){
+            if(!total[prop]){
+                total[prop] = {};
+            }
+            else if(!total[prop][arr[i][prop]]){
+                total[prop][arr[i][prop]] = 1;
+            }
+            else {
+                total[prop][arr[i][prop]]++;
+            }
+        }       
+    }
+    return total;
+};
+
+console.log(totalTaxa(animals));
