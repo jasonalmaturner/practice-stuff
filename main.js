902cc87c165b06830dfcc1118b48a8c7ce0afff0
Jason Turner <jasonalmaturner@gmail.com>
2014-10-28 10:50:53 -0600

    added another toy problem

--- main.js
+++ main.js
@@ -357,3 +357,57 @@ var isPalindrome = function(str){
 console.log(isPalindrome("Anna"));
 console.log(isPalindrome("Hello World"));
 console.log(isPalindrome("Amen icy cinema"));
+
+
+//create a function that takes an array of objects as an argument and a string that represents
+// a property of an object contained in the array and returns an object whose keys are the
+// properties and whose values are arrays of objects that match that property.
+
+//e.g.
+var arr = [
+    {name: 'Joe Montana', team: '49ers'}, 
+    {name: 'Jerry Rice', team: '49ers'}, 
+    {name: 'Steve Young', team: '49ers'},
+    {name: 'Tony Romo', team: 'Cowboys'},
+    {name: 'Fred Biletnikoff', team: 'Raiders'},
+    {name: 'Dick Butkus', team: 'Bears'},
+    {name: 'Emmitt Smith', team: 'Cowboys'},
+];
+    
+var objectifier = function(arr, str) {
+    debugger;
+    for(var i = 0; i < arr.length; i++){
+        for(key in arr[i]){
+            if(arr[i].key === str){
+                var newObj = {};
+                newObj.str = [newObj.str.push(arr[i])]
+                return newObj;
+            }
+        }
+    }
+}
+
+console.log(objectifier(arr, 'team'));
+    
+//should return:
+{ 
+    '49ers': [{name: 'Joe Montana', team: '49ers'}, {name: 'Jerry Rice', team: '49ers'}, {name: 'Steve Young', team: '49ers'}],
+    'Cowboys': [{name: 'Tony Romo', team: 'Cowboys'}, {name: 'Emmitt Smith', team: 'Cowboys'}],
+    'Bears': [{name: 'Dick Butkus', team: 'Bears'}],
+    'Raiders': [{name: 'Fred Biletnikoff', team: 'Raiders'}]
+}
+
+var objectifier = function(arr, str) {
+    var result = {}
+    for(var i = 0; i < arr.length; i++){
+        if(result[arr[i][str]]){
+            result[arr[i][str]].push(arr[i]);
+        }
+        else {
+            result[arr[i][str]] = [arr[i]];
+        }
+    }
+    return result;
+}
+
+// Review bracket notation versus dot notation
