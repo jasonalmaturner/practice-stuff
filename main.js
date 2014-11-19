8caf0353dc1c442e75f520ddd6a30265789bdc62
Jason Turner <jasonalmaturner@gmail.com>
2014-11-19 09:50:27 -0700

    added the most difficult and mind warping problem I've ever encountered. I didn't figure it out. I need to wrap my head around the answer before I can try to understand it.

--- main.js
+++ main.js
@@ -735,3 +735,34 @@ fizzBuzzEnhanced();
 
 // Same. Use an array, but use shift and unshift. Or, use push and pop. Actually, using push and pop makes a little more sense.
 // Undo list. For example, undo functions in editing a document recalls data like a stack.
+
+function delegate (child) {
+  return function anon(){
+    var mathObj = this[child];
+    for(var key in this){
+      if(this[key] === anon){
+        return mathObj[key].apply(mathObj, arguments);
+      }
+    }
+  }
+};
+ 
+// DON'T TOUCH ANY CODE BELOW THIS
+var obj = {
+  math: {
+    x: 2,
+ 
+    add: function (y) {
+      return this.x + y;
+    },
+ 
+    multiply: function (y) {
+      return this.x * y;
+    }
+  },
+  add: delegate('math'),
+  multiply: delegate('math')
+};
+
+obj.add(1) // 3
+obj.multiply(3) // 6
