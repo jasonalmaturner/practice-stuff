987f1235276af1f5155803946d8086ca4821ac52
Jason Turner <jasonalmaturner@gmail.com>
2015-01-11 21:20:56 -0700

    added another toy problem

---
+++ main2.js
@@ -0,0 +1,25 @@
+// “Write a function called finder that accepts two parameters, 
+// the first one is a string and the second is an array. 
+// Have your function return true if the string is found in the array and false if it is not”
+// var nums = [“hello”, “javascript”, “awesome”];
+// finder(“javascript”, nums); //true
+// finder(“lame”, nums); //false
+
+var pets = ['dog', 'cat', 'red breasted merganser', 'duck billed platypus', 'emu', 'hedgehog']
+
+var finder = function (string, array) {
+	for(var i = 0; i < array.length; i++) {
+		if(array[i] === string) {
+			return true;
+		}
+	}
+	return false;
+}
+
+var finder2 = function(string, array) {
+	if(array.indexOf(string) !== -1) {
+		return true;
+	} else {
+		return false;
+	}
+}
