6b96a9a5b7bdf48eab25f91bbe68e1c2100abed6
Jason Turner <jasonalmaturner@gmail.com>
2015-01-12 17:20:19 -0700

    added another toy problem

--- main2.js
+++ main2.js
@@ -1,3 +1,6 @@
+// Work on this toy problem until 9:20ish. We'll review a couple of solutions until 9:30,
+// and then work on the project review until 10:00
+
 // “Write a function called finder that accepts two parameters, 
 // the first one is a string and the second is an array. 
 // Have your function return true if the string is found in the array and false if it is not”
@@ -5,7 +8,7 @@
 // finder(“javascript”, nums); //true
 // finder(“lame”, nums); //false
 
-var pets = ['dog', 'cat', 'red breasted merganser', 'duck billed platypus', 'emu', 'hedgehog']
+var pets = ['dog', 'cat', 'red breasted merganser', 'duck billed platypus', 'emu', 'hedgehog'];
 
 var finder = function (string, array) {
 	for(var i = 0; i < array.length; i++) {
